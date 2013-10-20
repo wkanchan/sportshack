0697cf8ee4c76eedb277fd41fd2b8386557a8e92
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-10-20 14:30:36 -0700

    init the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportAnalytics::Application.config.secret_token = '45ca8c782e60342827a7fbf0d61ff42866050d3fbe8db373993ea0db2ec1a965784dfd4cf1b37cdd74ac3e66f084aec0b31245945c4bcd1387efa192b8ffdfb9'
